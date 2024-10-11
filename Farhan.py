85e36f0f0e179d2f81f124609973fbea6231ff11
MUHAMMAD FARHAN <muhammadfarhan99001@gmail.com>
2024-10-11 22:08:52 +0400

    Add files via upload
    Print Statement Task

---
+++ Farhan.py
@@ -0,0 +1,53 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("Hello, World!")
+... Hello, World!
+... >>> print(5 + 3)  # Output: 8
+... 8
+... >>> print("Python", "is", "fun!")  # Output: Python is fun!
+... Python is fun!
+... >>> print("She said, \"Hello!\"")  # Output: She said, "Hello!"
+... She said, "Hello!"
+... >>> print("She said, \"Hello!\"")  # Output: She said, "Hello!"
+... She said, "Hello!"
+... >>> print("First line\nSecond line")  # Output: First line (newline) Second line
+... First line
+... Second line
+... >>> print("Item 1\tItem 2\tItem 3")  # Output: Item 1   Item 2   Item 3
+... Item 1  Item 2  Item 3
+... >>> print("The result is", 42)  # Output: The result is 42
+... The result is 42
+... >>> print(12345)  # Output: 12345
+... 12345
+... >>> print(3.14159)  # Output: 3.14159
+... 3.14159
+... >>> print("Dollar sign: $")  # Output: Dollar sign: $
+... Dollar sign: $
+... >>> print("apple", "banana", "cherry", sep=", ")  # Output: apple, banana, cherry
+... apple, banana, cherry
+... >>> print("Hello,", end=" ")
+... >>> print("World!")  # Output: Hello, World! (on the same line)
+... World!
+... >>> print("Python! " * 3)  # Output: Python! Python! Python!
+... Python! Python! Python!
+... >>> print("\u2764")  # Output:  (heart symbol)
+... ❤
+... >>> print()  # Output: (just a blank line)
+... 
+... >>> print("Hello " + "World!")  # Output: Hello World!
+... Hello World!
+... >>> print(True)   # Output: True
+... True
+... >>> print(False)  # Output: False
+... False
+>>> print(1.2e3)  # Output: 1200.0
+1200.0
+>>> print("""This is a multiline
+... print statement.""")  # Output: This is a multiline
+This is a multiline
+print statement.
+>>>                       #         print statement.
+>>> print("This is a backslash: \\")  # Output: This is a backslash: \
+This is a backslash: \
